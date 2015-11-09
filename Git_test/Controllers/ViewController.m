8a8c234869a99f9f6d9cd1cd10de1c421609e8a2
kevin <akun1103@qq.com>
2015-11-09 15:35:42 +0800

    添加 AFNetworking

--- Git_test/Controllers/ViewController.m
+++ Git_test/Controllers/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import "AFHTTPRequestOperation.h"
+
 @interface ViewController ()
 
 @end
@@ -17,11 +19,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSString *str = [NSString stringWithFormat:@"https://alpha-api.app.net/stream/0/posts/stream/global"];
+    NSURL *url = [NSURL URLWithString:str];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+//    NSData *response = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    //IOS5自带解析类NSJSONSerialization从response中解析出数据放到字典中[NSURLSession dataTaskWithRequest:completionHandler:]
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request
+                                                completionHandler:
+                                      ^(NSData *data, NSURLResponse *response, NSError *error) {
+                                          // 输出返回的状态码，请求成功的话为200
+                                          NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
+                                           NSLog(@"dict = %@",dict);
+                                      }];
+    // 使用resume方法启动任务
+    [dataTask resume];
+    
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation,id responseObject){
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableLeaves error:nil];
+        NSLog(@"dict = %@",dict);
+    } failure:^(AFHTTPRequestOperation *operation,NSError *error){
+        NSLog(@"%@",error);
+    }];
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    [queue addOperation:operation];
 }
 
 @end
